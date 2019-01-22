e92b4dd78fd9b946866d479cb260f084a145ec42
Riedel <riedelgcamila@gmail.com>
2019-01-22 00:54:57 -0300

    Added slide right capabilities. Missing: Sliding left

--- app.js
+++ app.js
@@ -17,8 +17,55 @@ var mtl = document.getElementsByClassName("overlay-mtl")[0];
 var inkdrink = document.getElementsByClassName("overlay-inkdrink")[0];
 var other = document.getElementsByClassName("overlay-other")[0];
 
-var projectOrder = [themcomp,fontdatabase,vanish,solemedia,spur,mtl,inkdrink,other];
+/*var projectOrder = [themcomp,fontdatabase,vanish,solemedia,spur,mtl,inkdrink,other];*/
 
-function slideRight(project){
-	
+var projectOrder = ["overlay-themcomp",
+"overlay-fontdatabase",
+"overlay-vanish",
+"overlay-solemedia",
+"overlay-spur",
+"overlay-mtl",
+"overlay-inkdrink",
+"overlay-other"]
+
+function slideRight(shouldGoTo){
+	var currentSlideNumber = projectOrder.indexOf(shouldGoTo) - 1;
+	//console.log(currentSlideNumber);
+	var currentSlide = projectOrder[currentSlideNumber];
+	//console.log(currentSlide);
+	closeOverlay(currentSlide);
+	//closeOverlay('overlay-themcomp');
+	openOverlay(shouldGoTo);
+	//openOverlay('overlay-fontdatabase');
 }
+
+function slideLeft(shouldGoTo){
+	//previous slide. opposite of slideright
+}
+
+
+/*ño
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  slides[slideIndex-1].style.width = "100%";
+  slides[slideIndex-2].style.width = "0%";
+}*/
