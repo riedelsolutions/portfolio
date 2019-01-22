2657ee5dda9032fd04a100e44c837ba714273bf6
Riedel <riedelgcamila@gmail.com>
2019-01-21 23:27:43 -0300

    Added most overlays, missing slideshow actions

--- app.js
+++ app.js
@@ -7,3 +7,18 @@ function openOverlay(project) {
 function closeOverlay(project) {
   document.getElementsByClassName(project)[0].style.width = "0%";
 }
+
+var themcomp = document.getElementsByClassName("overlay-themcomp")[0];
+var fontdatabase = document.getElementsByClassName("overlay-fontdatabase")[0];
+var vanish = document.getElementsByClassName("overlay-vanish")[0];
+var solemedia = document.getElementsByClassName("overlay-solemedia")[0];
+var spur = document.getElementsByClassName("overlay-spur")[0];
+var mtl = document.getElementsByClassName("overlay-mtl")[0];
+var inkdrink = document.getElementsByClassName("overlay-inkdrink")[0];
+var other = document.getElementsByClassName("overlay-other")[0];
+
+var projectOrder = [themcomp,fontdatabase,vanish,solemedia,spur,mtl,inkdrink,other];
+
+function slideRight(project){
+	
+}
